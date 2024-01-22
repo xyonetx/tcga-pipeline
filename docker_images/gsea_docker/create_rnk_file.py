2a43ee8c81274f5f7ea5b05d51e58d2c2d4a310b
Brian <blawney@hsph.harvard.edu>
2024-01-22 09:56:35 -0500

    Fixing rnk creation file for case when hard zero is given to p-value. Removes Inf

--- docker_images/gsea_docker/create_rnk_file.py
+++ docker_images/gsea_docker/create_rnk_file.py
@@ -17,4 +17,13 @@ if __name__ == '__main__':
     dge_df = pd.read_table(args.input_file, index_col=0)
     dge_df['rnk'] = -np.log(dge_df['pvalue']) * dge_df['log2FoldChange']
     rnk_df = dge_df['rnk'].dropna().sort_values(ascending=False)
-    rnk_df.to_csv(args.output, sep='\t', header=False)
+    # need to handle cases where the FDR is a hard zero and we get inf:
+    finite_idx = np.isfinite(rnk_df)
+    if finite_idx.sum() < rnk_df.shape[0]:
+        finite_entries = rnk_df.loc[finite_idx]
+        top_finite = finite_entries.iloc[0]
+        bottom_finite = finite_entries.iloc[len(finite_entries)-1]
+        rnk_df.loc[(~finite_idx) & (rnk_df > 0)] = top_finite
+        rnk_df.loc[(~finite_idx) & (rnk_df < 0)] = bottom_finite
+
+    rnk_df.to_csv(args.output, sep='\t', header=False
